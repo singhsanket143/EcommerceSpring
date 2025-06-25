817e952105f4a13ff71dba0cdf5ef0f6e0c8788e
Pritesh Kumar <pritesh.kumar2@gmail.com>
2025-06-25 17:04:12 +0530

    use api url from app.properties

--- src/main/java/org/example/ecommercespring/configuration/RetrofitConfig.java
+++ src/main/java/org/example/ecommercespring/configuration/RetrofitConfig.java
@@ -2,6 +2,7 @@ package org.example.ecommercespring.configuration;
 
 import io.github.cdimascio.dotenv.Dotenv;
 import org.example.ecommercespring.gateway.api.FakeStoreCategoryApi;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import retrofit2.Retrofit;
@@ -10,13 +11,15 @@ import retrofit2.converter.gson.GsonConverterFactory;
 @Configuration
 public class RetrofitConfig {
 
+    @Value("${fake-store.apiurl}")
+    private String fakestoreapiUrl;
 
     @Bean
     public Retrofit retrofit() {
-        Dotenv dotenv = Dotenv.load();
-        String baseUrl = dotenv.get("FAKESTOREAPIURL");
+        //Dotenv dotenv = Dotenv.load();
+        //String baseUrl = dotenv.get("FAKESTOREAPIURL");
         return new Retrofit.Builder()
-                .baseUrl(baseUrl)
+                .baseUrl(fakestoreapiUrl)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
     }
