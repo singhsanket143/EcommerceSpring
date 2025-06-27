ef44a44d6f31ccd0775a391a3a6ab7d5607f3a8f
Sai Sri Harsha <ssharsha333@gmail.com>
2025-06-28 00:58:01 +0530

    Updated code to store baseUrl in .env and Integrate APIs Call for Get products of category

--- src/main/java/org/example/ecommercespring/gateway/IProductGateway.java
+++ src/main/java/org/example/ecommercespring/gateway/IProductGateway.java
@@ -1,8 +1,14 @@
 package org.example.ecommercespring.gateway;
 
 import org.example.ecommercespring.dto.ProductDTO;
+import org.example.ecommercespring.dto.ProductsItem;
+
+import java.io.IOException;
+import java.util.List;
 
 public interface IProductGateway {
 
     ProductDTO getProductById(Long id) throws Exception;
+
+    public List<ProductsItem> getProductByCategory(String type) throws IOException;
 }
--- src/main/java/org/example/ecommercespring/services/FakeStoreProductService.java
+++ src/main/java/org/example/ecommercespring/services/FakeStoreProductService.java
@@ -1,8 +1,12 @@
 package org.example.ecommercespring.services;
 
 import org.example.ecommercespring.dto.ProductDTO;
+import org.example.ecommercespring.dto.ProductsItem;
 import org.example.ecommercespring.gateway.IProductGateway;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.List;
 
 @Service
 public class FakeStoreProductService implements IProductService {
@@ -16,5 +20,10 @@ public class FakeStoreProductService implements IProductService {
     @Override
     public ProductDTO getProductById(Long id) throws Exception {
         return this.productGateway.getProductById(id);
+    }
+
+    @Override
+    public List<ProductsItem> getProductByCategory(String type) throws IOException {
+        return this.productGateway.getProductByCategory(type);
     }
 }
