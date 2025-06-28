8b10895686ae6453f0310827fc8b3ce7c0fa2f22
Naman garg <naman.2327cseml1022@example.com>
2025-06-28 14:56:05 +0530

    Add product category API using OkHttp

--- src/main/java/org/example/ecommercespring/configuration/RetrofitConfig.java
+++ src/main/java/org/example/ecommercespring/configuration/RetrofitConfig.java
@@ -2,6 +2,7 @@ package org.example.ecommercespring.configuration;
 
 import org.example.ecommercespring.gateway.api.FakeStoreCategoryApi;
 import org.example.ecommercespring.gateway.api.FakeStoreProductApi;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import retrofit2.Retrofit;
@@ -10,11 +11,12 @@ import retrofit2.converter.gson.GsonConverterFactory;
 @Configuration
 public class RetrofitConfig {
 
-
+@Value("${BASE_URL}")
+private String baseUrl;
     @Bean
     public Retrofit retrofit() {
         return new Retrofit.Builder()
-                .baseUrl("https://fakestoreapi.in/api/")
+                .baseUrl(baseUrl)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
     }
