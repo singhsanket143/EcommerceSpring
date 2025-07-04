de11ba063d39249629120fefc1faf3fc03253cb2
riyabansal <riyabansal98@gmail.com>
2025-07-04 14:00:03 +0100

    Added native queries

--- src/main/java/org/example/ecommercespring/repository/ProductRepository.java
+++ src/main/java/org/example/ecommercespring/repository/ProductRepository.java
@@ -2,8 +2,18 @@ package org.example.ecommercespring.repository;
 
 import org.example.ecommercespring.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @Query("SELECT p FROM Product p WHERE p.price > :minPrice")
+    List<Product> findExpensiveProducts(@Param("minPrice") double minPrice);
+
+    @Query(value = "SELECT * FROM product WHERE MATCH(name, description) AGAINST (:keyword)", nativeQuery = true)
+    List<Product> searchFullText(@Param("keyword") String keyword);
 }
